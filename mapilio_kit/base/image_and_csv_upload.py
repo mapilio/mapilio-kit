08527482522b0f985e3176a94e848314a99a65b0
gorkemgul <gorkemgtech@gmail.com>
2023-09-12 09:10:51 +0300

    fix typo

--- mapilio_kit/base/image_and_csv_upload.py
+++ mapilio_kit/base/image_and_csv_upload.py
@@ -5,11 +5,11 @@ class image_and_csv_upload:
     help = "process panoramic images and upload to Mapilio"
 
     def fundamental_arguments(self, parser: argparse.ArgumentParser):
-        from . import loader, CSVprocesser
-        CSVprocesser().fundamental_arguments(parser)
+        from . import loader, CSVprocessor
+        CSVprocessor().fundamental_arguments(parser)
         loader().fundamental_arguments(parser)
 
     def perform_task(self, args: dict):
-        from . import loader, CSVprocesser
-        CSVprocesser().perform_task(args)
+        from . import loader, CSVprocessor
+        CSVprocessor().perform_task(args)
         loader().perform_task(args)
