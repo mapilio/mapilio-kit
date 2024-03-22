a5e216f1918823672171d442bc98fee118726861
Görkem Gül <104680758+gorkemgul@users.noreply.github.com>
2024-03-22 12:40:05 +0300

    Update max_extractor_install.sh

--- max_extractor_install.sh
+++ max_extractor_install.sh
@@ -5,7 +5,7 @@ mkdir -p dependencies
 #
 # install EAC to Equirectangular conversion binary
 #
-git -C dependencies  clone https://github.com/mcvarer/max2sphere-batch
+git -C dependencies  clone https://github.com/mapilio/max2sphere-batch.git
 
 make -C dependencies/max2sphere-batch -j4
 
