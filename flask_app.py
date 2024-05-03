7bb849f3894c216811eef2c8fc1c38b8e884d8e2
Yusuf Esat <xyusufesat@gmail.com>
2024-05-03 15:35:48 +0300

    Flask web interface added.

---
+++ flask_app.py
@@ -0,0 +1,165 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flaskwebgui import FlaskUI
+import webbrowser
+import os
+import subprocess
+import shutil
+import re
+
+from mapilio_kit.base import authenticator
+from mapilio_kit.components.edit_config import edit_config
+from mapilio_kit.components.login import list_all_users
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.join(os.path.expanduser("~"), ".cache", "mapilio", "MapilioKit", "images/")
+
+
+def get_args_mapilio(func):
+    arg_names = func.__code__.co_varnames[:func.__code__.co_argcount]
+    return {arg: None for arg in arg_names}
+
+
+def check_authenticate():
+    global authentication_status, token
+    if len(list_all_users()) == 0:
+        authentication_status = False
+        token = None
+    elif len(list_all_users()) >= 2:
+        token = None
+        authentication_status = False
+        username = input("Found multiple Mapilio accounts. Please specify your username.\n")
+    else:
+        token = list_all_users()[0]['user_upload_token']
+        authentication_status = True
+
+    return token, authentication_status
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    token, authentication_status = check_authenticate()
+    if authentication_status:
+        return render_template("upload.html", token=token)
+    else:
+        return render_template('login.html')
+
+
+@app.route('/login', methods=['GET','POST'])
+def mapilio_login():
+
+    if request.method == 'POST':
+        args = get_args_mapilio(edit_config)
+        email = request.form['email'].strip()
+        password = request.form['password'].strip()
+
+        username = email.split('@')[0]
+
+        args["user_name"] = username
+        args["user_email"] = email
+        args["user_password"] = str(password)
+        args["gui"] = True
+        check_authenticate = authenticator().perform_task(args)
+
+        if check_authenticate['status']:
+            message = check_authenticate['message']
+            token = check_authenticate['token']
+            return render_template("upload.html", message=message, token=token)
+        else:
+            message = check_authenticate['message']
+            return render_template('login.html', message=message)
+    else:
+        return render_template('login.html')
+
+
+@app.route('/logout', methods=['GET'])
+def remove_accounts():
+    MAPILIO_CONFIG_PATH = os.getenv(
+        "MAPILIO_CONFIG_PATH",
+        os.path.join(
+            os.path.expanduser("~"),
+            ".config",
+            "mapilio",
+            "configs",
+            "CLIENT_USERS",
+        ),
+    )
+    if os.path.exists(MAPILIO_CONFIG_PATH):
+        os.remove(MAPILIO_CONFIG_PATH)
+    else:
+        return jsonify(success=True, message="No accounts found!"), 200
+    return jsonify(success=True, message="Account successfully removed!"), 200
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def mapilio_upload_page():
+    if request.method == 'GET':
+        token, authentication_status = check_authenticate()
+
+        if authentication_status:
+            return render_template('upload.html', token=token)
+        else:
+            return redirect(url_for("mapilio_login"))
+    elif request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify(success=False, message="No file part")
+
+        for file in request.files.getlist('file'):
+            if file.filename == '':
+                continue
+            if not os.path.exists(UPLOAD_FOLDER):
+                os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+            file.save(UPLOAD_FOLDER + file.filename)
+
+        command = f"mapilio_kit upload {UPLOAD_FOLDER} --dry_run"
+        try:
+            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            if result.returncode == 0:
+                try:
+                    shutil.rmtree(UPLOAD_FOLDER)
+                    output_lines = result.stderr.split('\n')
+                    output_text = '\n'.join(output_lines)
+
+                    total_images_pattern = r'"total_images": (\d+)'
+                    processed_images_pattern = r'"processed_images": (\d+)'
+                    failed_images_pattern = r'"failed_images": (\d+)'
+
+                    total_images_match = re.search(total_images_pattern, output_text)
+                    processed_images_match = re.search(processed_images_pattern, output_text)
+                    failed_images_match = re.search(failed_images_pattern, output_text)
+
+                    total_images = int(total_images_match.group(1)) if total_images_match else 0
+                    processed_images = int(processed_images_match.group(1)) if processed_images_match else 0
+                    failed_images = int(failed_images_match.group(1)) if failed_images_match else 0
+
+                    return jsonify(success=True, message="Images uploaded successfully", total_images=total_images, processed_images=processed_images, failed_images=failed_images), 200
+                except OSError as err:
+                    print(f"Error: {UPLOAD_FOLDER} could not be deleted. - {err}")
+        except subprocess.CalledProcessError as e:
+            return jsonify(success=False, message="Error occurred while running command"), 500
+    else:
+        return jsonify(success=False, message="Method Not Allowed"), 500
+
+@app.route('/support', methods=['GET', 'POST'])
+def mapilio_support_page():
+    token, authentication_status = check_authenticate()
+
+    if authentication_status:
+        return render_template('support.html', token=token)
+    else:
+        return redirect(url_for("mapilio_login"))
+
+@app.route('/about', methods=['GET', 'POST'])
+def mapilio_about_page():
+    token, authentication_status = check_authenticate()
+
+    if authentication_status:
+        return render_template('about.html', token=token)
+    else:
+        return redirect(url_for("mapilio_login"))
+
+if __name__ == "__main__":
+    # webbrowser.open("http://127.0.0.1:8080/")
+    app.run(host="0.0.0.0", port=8080, debug=True)
+    # FlaskUI(app=app, server="flask", port=5050, width=1200, height=800).run()
---
+++ static/scripts/main.js
@@ -0,0 +1,300 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const logoutBtn = document.getElementById('logoutBtn');
+    const dropArea = document.getElementById('drop-area');
+    const fileList = document.getElementById('fileList').querySelector('ul');
+
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false);
+    });
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    ['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false);
+    });
+
+    ['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false);
+    });
+
+    function highlight(e) {
+        dropArea.classList.add('highlight');
+    }
+
+    function unhighlight(e) {
+        dropArea.classList.remove('highlight');
+    }
+
+    function updateTotalImagesCount() {
+        const totalImagesCount = fileList.querySelectorAll('li').length;
+        document.getElementById('totalImagesCount').textContent = totalImagesCount;
+    }
+
+    dropArea.addEventListener('drop', handleDrop, false);
+
+    function handleDrop(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        let dt = e.dataTransfer;
+        console.log("Files from Drop:", dt.files);
+
+        let droppedFiles = dt.files; 
+
+
+        handleFiles(droppedFiles);
+        updateTotalImagesCount();
+
+
+        let newFileList = new DataTransfer();
+
+        for (let i = 0; i < fileInput.files.length; i++) {
+            newFileList.items.add(fileInput.files[i]);
+        }
+
+        for (let i = 0; i < droppedFiles.length; i++) {
+            newFileList.items.add(droppedFiles[i]);
+        }
+        fileInput.files = newFileList.files;
+    }
+
+
+
+    function isDuplicate(fileName) {
+        let items = fileList.querySelectorAll('li');
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].innerHTML.trim() === fileName.trim()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function handleFiles(droppedFiles) {
+        [...droppedFiles].forEach(file => {
+            if (!isDuplicate(file.name) && file.type.match('image.*')) {
+                uploadFile(file);
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: `Please drop only image files, and make sure the file does not already exist in the list.`,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    showConfirmButton: false
+                });
+            }
+        });
+        updateTotalImagesCount();
+    }
+
+    const fileInput = document.getElementById('fileElem');
+
+    fileInput.addEventListener('change', function () {
+        let files = this.files;
+        handleFiles(files);
+    }, false);
+
+    let addedFiles = [];
+
+   function uploadFile(file) {
+        let li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+
+        let imageContainer = document.createElement('div');
+        imageContainer.className = 'image-container';
+
+        let previewImg = document.createElement('img');
+        previewImg.className = 'preview-image float-start rounded me-2 img-fluid';
+        previewImg.src = URL.createObjectURL(file);
+
+        imageContainer.appendChild(previewImg);
+
+        li.appendChild(imageContainer);
+
+        let fileInfo = document.createElement('div');
+        fileInfo.className = 'text-end';
+
+        let fileNameSpan = document.createElement('span');
+        fileNameSpan.textContent = file.name;
+        fileNameSpan.className = 'text-center me-auto';
+        fileInfo.appendChild(fileNameSpan);
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.className = 'btn btn-danger btn-sm ms-2';
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.addEventListener('click', function () {
+            let index = addedFiles.indexOf(file);
+            if (index !== -1) {
+                addedFiles.splice(index, 1);
+            }
+            li.remove();
+            updateTotalImagesCount();
+            removeFileFromInput(file);
+        });
+        fileInfo.appendChild(deleteBtn);
+
+        li.appendChild(fileInfo);
+
+        fileList.appendChild(li);
+
+        addedFiles.push(file);
+    }
+
+
+    function removeFileFromInput(fileToRemove) {
+        let newFileList = new DataTransfer();
+        for (let i = 0; i < fileInput.files.length; i++) {
+            if (fileInput.files[i] !== fileToRemove) {
+                newFileList.items.add(fileInput.files[i]);
+            }
+        }
+        fileInput.files = newFileList.files;
+    }
+
+    logoutBtn.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        fetch('/logout', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Logged Out',
+                        text: 'You have been successfully logged out!',
+                        timer: 2000,
+                        timerProgressBar: true,
+                        showConfirmButton: false
+                    }).then(() => {
+                        window.location.href = '/login';
+                    });
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: data.message,
+                        timer: 2000,
+                        timerProgressBar: true,
+                        showConfirmButton: false
+                    });
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+    const uploadBtn = document.getElementById('uploadBtn');
+    uploadBtn.addEventListener('click', function () {
+        let files = fileInput.files;
+        console.log("Selected files:", files);
+        let formData = new FormData();
+
+        for (let i = 0; i < files.length; i++) {
+            formData.append('file', files[i]);
+        }
+        if (files.length === 0) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Please select a file to upload.',
+                timer: 2000,
+                timerProgressBar: true,
+                showConfirmButton: false
+            });
+            return;
+        }
+
+        Swal.fire({
+            icon: 'info',
+            title: 'Uploading...',
+            text: 'Please wait while we upload your file.',
+            showConfirmButton: false,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false
+        });
+
+        fetch('/upload', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    fileList.innerHTML = '';
+                    fileInput.value = '';
+                    updateTotalImagesCount();
+                    Swal.fire({
+                        icon: 'success',
+                        title: "Upload successfully done!",
+                        html: "<table style='margin: 0 auto;'> \
+                                  <tr> \
+                                      <td style='text-align: left;'>Total images:</td> \
+                                      <td style='padding-left: 10px; font-weight: bold;'>" + data.total_images + "</td> \
+                                  </tr> \
+                                  <tr> \
+                                      <td style='text-align: right;'>Processed images:</td> \
+                                      <td style='padding-left: 10px; font-weight: bold;'>" + data.processed_images + "</td> \
+                                  </tr> \
+                                  <tr> \
+                                      <td style='text-align: left;'>Failed images:</td> \
+                                      <td style='padding-left: 10px; font-weight: bold;'>" + data.failed_images + "</td> \
+                                  </tr> \
+                              </table>",
+                        footer: "Thanks for contributions to Mapilio 🎉",
+                        timerProgressBar: true,
+                        showConfirmButton: false
+                    })
+                                 
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: data.message,
+                        timer: 2000,
+                        timerProgressBar: true,
+                        showConfirmButton: false
+                    });
+                }
+            })
+            .catch(data => {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: data.message,
+                    timer: 2000,
+                    timerProgressBar: true,
+                    showConfirmButton: false
+                });
+            });
+    });
+
+    const removeAllBtn = document.getElementById('removeAllBtn');
+
+    removeAllBtn.addEventListener('click', function () {
+        if (fileList.innerHTML === '' && fileInput.value === '') {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Warning',
+                text: 'The file list is already empty!',
+                timer: 2000,
+                timerProgressBar: true,
+                showConfirmButton: false
+            });
+        } else {
+            fileList.innerHTML = '';
+            fileInput.value = '';
+            updateTotalImagesCount();
+        }
+    });
+});
