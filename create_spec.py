7089e1f288e8b6df741456a35cb166395dbcb928
Yusuf Esat <xyusufesat@gmail.com>
2024-05-23 14:33:15 +0300

    Creating a dynamic spec file [TEST]

--- create_spec.py
+++ create_spec.py
@@ -3,31 +3,71 @@ import subprocess
 import sys
 from pathlib import Path
 
+
 def get_installed_package_path(package_name):
     result = subprocess.run([sys.executable, '-m', 'pip', 'show', package_name], capture_output=True, text=True)
     if result.returncode != 0:
         raise ValueError(f"Package {package_name} not found")
+
+    location = None
     for line in result.stdout.splitlines():
         if line.startswith('Location:'):
-            return line.split(' ', 1)[1]
-    raise ValueError(f"Location not found for package {package_name}")
+            location = line.split(' ', 1)[1].strip()
+            break
+
+    if not location:
+        raise ValueError(f"Location not found for package {package_name}")
+
+    package_folder_name = package_name.split('-')[0]
+    package_path = Path(location) / package_folder_name
+    if package_path.exists():
+        return package_path
+
+    package_folder_name = package_name.replace('-', '_')
+    package_path = Path(location) / package_folder_name
+    if package_path.exists():
+        return package_path
+
+    package_path_with_py = package_path.with_suffix('.py')
+    if package_path_with_py.exists():
+        return package_path_with_py
+
+    if package_name == 'attrs':
+        alt_package_name = 'attr'
+        alt_package_path = Path(location) / alt_package_name
+        if alt_package_path.exists():
+            return alt_package_path
+
+    raise ValueError(f"Package path not found for {package_name}")
+
 
 def create_spec_file():
     requirements_file = 'requirements.txt'
     spec_file = 'flask_app.spec'
 
-    datas = []
+    datas = [('templates', 'templates'), ('static', 'static'), ('mapilio_kit', 'mapilio_kit')]
     hiddenimports = []
 
     with open(requirements_file) as f:
         packages = [line.split('==')[0].strip() for line in f if line.strip() and not line.startswith('#')]
+        print(packages)
 
     for package in packages:
         try:
+            if package == 'ExifRead':
+                package = 'exifread'
             package_path = get_installed_package_path(package)
             hiddenimports.append(package)
-            # Add the package path to datas
-            datas.append((package_path, package))
+
+            package_folder_name = package.replace('-', '_')
+
+            if package_folder_name == package:
+                package_data_name = package
+            else:
+                package_data_name = package_path.parts[-1]
+
+            datas.append((str(package_path), package_data_name))
+
         except ValueError as e:
             print(f"Warning: {e}")
 
@@ -37,9 +77,11 @@ def create_spec_file():
 
 block_cipher = None
 
+options = [("u", None, "OPTION")]
+
 a = Analysis(
     ['flask_app.py'],
-    pathex=['.'],
+    pathex=[SPECPATH],
     binaries=[],
     datas={datas},
     hiddenimports={hiddenimports},
@@ -55,12 +97,16 @@ pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
 exe = EXE(
     pyz,
     a.scripts,
+    options,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
     [],
-    exclude_binaries=True,
     name='MapilioKit-Flask',
     debug=False,
     strip=False,
     upx=True,
+    runtime_tmpdir=None,
     console=True,
 )
 
