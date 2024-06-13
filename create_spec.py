b1edfdd0337833de2e8fd71026886bbc72c6c5fc
Yusuf Esat <xyusufesat@gmail.com>
2024-06-13 09:51:11 +0300

    mapilio-kit path changed to working directory

--- create_spec.py
+++ create_spec.py
@@ -77,7 +77,8 @@ def create_spec_file():
     requirements_file = 'requirements.txt'
     spec_file = 'flask_app.spec'
 
-    datas = [('templates', 'templates'), ('static', 'static'), ('mapilio_kit', 'mapilio_kit')]
+    current_directory = os.getcwd()
+    datas = [('templates', 'templates'), ('static', 'static'), ({current_directory}, 'mapilio_kit')]
     hiddenimports = ['configparser']
 
     # ExifTool'u kur ve yolunu bul
